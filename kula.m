43808d12f77a7a8ed33912777669abd80a3a9a60
mikimaxS <126713299+mikimaxS@users.noreply.github.com>
2023-03-08 13:10:25 +0100

    tutaj zaczyna animacje

--- kula.m
+++ kula.m
@@ -11,9 +11,9 @@ r = 6371;
 
 %generate the x and y cordinates for the circle
 [x,y,z] = sphere(numPoints);
-x = x*r;
-y = y*r;
-z = z*r;
+x = center(1) + x*r;
+y = center(1) + y*r;
+z = center(1) + z*r;
 
 surf(x,y,z);
 shading ('interp');                 % Interpolation   
@@ -21,25 +21,62 @@ axis equal
 hold on;
 
 
-%MOON'S ORBIT 
-R = 384748; % major radius
-r = 400; % minor radius
+%MOON'S ORBIT  
 % Define the lengths of the major and minor axes
 a = 384748;
 b = 363300;
 
-theta   = linspace(0, 2*pi, numPoints);    % angle for major radius
-phi     = linspace(0, 2*pi, numPoints);    % angle for minor radius
+% Define the angle of rotation for the ellipse
+theta = pi/4;
 
-[THETA,PHI] = meshgrid(theta,phi);
+% Generate the x and y coordinates for the ellipse
+t = linspace(0, 2*pi, numPoints);
+x = center(1) + a*cos(t)*cos(theta) - b*sin(t)*sin(theta);
+y = center(2) + a*cos(t)*sin(theta) + b*sin(t)*cos(theta);
 
-x = (center(1) + a*cos(PHI)*cos(PHI) - b*sin(PHI)*sin(PHI)).*cos(THETA);
-y = (center(2) + a*cos(PHI)*sin(PHI) + b*sin(PHI)*cos(PHI)).*sin(THETA);
-
-%x = (R + r*cos(PHI)).*cos(THETA);
-%y = (R + r*cos(PHI)).*sin(THETA);
-z = r*sin(PHI);
-
-surf(x,y,z)
+% Plot the ellipse
+plot(x, y, 'b--', LineWidth=0.1);
+hold on;
+axis equal;
+grid on;
 shading ('interp'); 
+
+%MOON ITSELF
+%Moon's radius
+r = 1737;
+
+%CENTER
+center = [x(1) y(1) 0];
+
+%generate the x and y cordinates for the circle
+[m, n, o] = sphere(numPoints);
+m = center(1) + m*r;
+n = center(2) + n*r;
+o = center(3) + o*r;
+
+surf(m, n, o, "FaceColor","interp");    % Interpolation   
+axis equal
+colormap = [0.7 0.7 1];          
 hold off;
+
+
+%ANIMATION
+%while true 
+%MOON ITSELF
+%Moon's radius
+r = 1737;
+
+%CENTER
+center = [x(1) y(1) 0];
+
+%generate the x and y cordinates for the circle
+[m, n, o] = sphere(numPoints);
+m = center(1) + m*r;
+n = center(2) + n*r;
+o = center(3) + o*r;
+
+surf(m, n, o, "FaceColor","interp");    % Interpolation   
+axis equal
+colormap = [0.7 0.7 1];       
+hold off;
+
